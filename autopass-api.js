00195397db156e44ce7cc585f1c05fed70ca2e7f
toby <toby@cardbo.info>
2020-08-10 10:23:36 +0000

    getUsers getBanks getCards updateUser function complete

--- autopass-api.js
+++ autopass-api.js
@@ -9,18 +9,7 @@ const client = new Client({
     password: "kl;'",
 });
 
-const pool = new Pool({
-    user: 'awesdjzo',
-    database: 'autopass-db',
-    password: "kl;'",
-});
-
 client.connect((err, client, done) => {
-    if (err) throw err
-    console.log("Database Connected!");
-});
-
-pool.connect((err, client, done) => {
     if (err) throw err
     console.log("Database Connected!");
 });
@@ -62,14 +51,36 @@ router.get('/getofferdata-by-cardname', (apiReq, apiRes) => {
     }
 });
 
-router.get('/getcards', (apiReq, apiRes) => {
-    if (apiReq.query.key === "C5tyrugsa5M3qLYaPxvJ5QfqKiTIxAwem4Jz3vSbxKS73ZIzvCw6HJbsRoYer0cT") {
-        client.query(`SELECT * FROM cards`, (err, res) => {
+router.get('/getusers', (apiReq, apiRes) => {
+    if (apiReq.query.key === "JPT6RFfAxOPzfm640HOxb7l7IIlJeLQwSqIkilgORUmg3vkafacl8C5MKNn1bZ9P") {
+        client.query(`SELECT * FROM users`, (err, res) => {
             if (err) {
                 console.log(err);
                 apiRes.json(err);
             } else {
                 apiRes.json(res.rows);
+            }
+        });
+    } else {
+        apiRes.json("Api Key Error!");
+    }
+});
+
+router.get('/getcards', (apiReq, apiRes) => {
+    if (apiReq.query.key === "C5tyrugsa5M3qLYaPxvJ5QfqKiTIxAwem4Jz3vSbxKS73ZIzvCw6HJbsRoYer0cT") {
+        client.query(`SELECT * FROM cards`, async (err, res) => {
+            if (err) {
+                console.log(err);
+                apiRes.json(err);
+            } else {
+                let cardData = res.rows;
+                for (let i = 0; i < cardData.length; ++i) {
+                    const { rows } = await client.query(`SELECT * FROM banks WHERE id='${cardData[i].bankid}'`);
+                    cardData[i].bankname = await rows[0].bankname;
+                    cardData[i].bankimage = await rows[0].bankimage;
+                }
+                 console.log(cardData);
+                 apiRes.json(cardData);
             }
         });
     } else {
@@ -130,6 +141,7 @@ router.post('/update-user', (apiReq, apiRes) => {
     const displayName = apiReq.body.displayName;
     const addCards = apiReq.body.addCards;
     const deleteCards = apiReq.body.deleteCards;
+
     if (lineid) {
         if (apiReq.query.key === "UPEvbZ8dPPc7JABil1FVvnufbMniK9dTs3JFMq6wm2kXx6WQ53Pd5boftdzNka0T") {
             client.query(`SELECT * FROM users WHERE lineid='${lineid}'`, (err, res) => {
@@ -147,7 +159,32 @@ router.post('/update-user', (apiReq, apiRes) => {
                             }
                         });
                     } else {
-                        apiRes.json(res.rows[0]);
+                        const userid = res.rows[0].id;
+                        let queryString = `UPDATE users SET `;
+                        if (email) {
+                            queryString = queryString + `email='${email}',`;
+                        }
+                        if (displayName) {
+                            queryString = queryString + `displayName='${displayName}',`;
+                        }
+                        if (addCards) {
+                            const new_string = addCards.length > 1 ? `{${[...addCards]}}` : addCards.length > 0 ? `${addCards[0]}` : null;
+                            if (new_string) queryString = queryString + `owncards=array_cat(owncards, '${new_string}'),`;
+                        }
+                        if (deleteCards) {
+                            const new_string = deleteCards.length > 1 ? `{${[...deleteCards]}}` : deleteCards.length > 0 ? `${deleteCards[0]}` : null;
+                            if (new_string) queryString = queryString + `owncards=array_remove(owncards, '${new_string}'),`;
+                        }
+                        queryString = queryString.slice(0, -1) + ` WHERE id='${userid}' RETURNING *`;
+                        // console.log(queryString);
+                        client.query(queryString, (inserUsererr, inserUserres) => {
+                            if (inserUsererr) {
+                                console.log(inserUsererr);
+                                apiRes.json(inserUsererr);
+                            } else {
+                                apiRes.json(inserUserres.rows[0]);
+                            }
+                        });
                     }
                 }
             });
